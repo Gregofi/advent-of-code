eec65e3cefec16f76ce7fdaf2322dd5e695682ca
Filip Gregor <filip.gregor98@gmail.com>
2022-12-01 22:36:07 +0100

    Solution to first

--- 01/main.hs
+++ 01/main.hs
@@ -6,23 +6,35 @@ prefix (h1:t1) (h2:t2)
     | otherwise = False
 
 
-remove_prefix :: String -> String -> String
-remove_prefix [] s = s
-remove_prefix _ [] = []
-remove_prefix (h1:t1) (h2:t2)
-    | h1 == h2 = remove_prefix t1 t2
+removePrefix :: String -> String -> String
+removePrefix [] s = s
+removePrefix _ [] = []
+removePrefix (h1:t1) (h2:t2)
+    | h1 == h2 = removePrefix t1 t2
     | otherwise = h2:t2
 
 
 split :: String -> String -> [String]
-split _ [] = []
-split delim str | (prefix delim str) = []:(split delim (remove_prefix delim str))
-split delim (h:t) = let head:tail = split delim t
-                    in (h:head):tail
+split _ "" = []
+split delim str | prefix delim str = "":split delim (removePrefix delim str)
+split delim (h:t) = case split delim t of
+                        [] -> [h:""]
+                        (head:tail) -> (h:head):tail
 
+
+max' :: Integer -> Integer -> Integer
+max' a b | a > b = a | otherwise = b
+
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) =
+    let smaller = filter (>= x) xs
+        bigger  = filter (< x) xs
+    in qsort smaller ++ [x] ++ qsort bigger
 
 main = do
     content <- getContents
-    let str_lst = split "\n\n" content
-        str_lss = split "\n" $ head str_lst
-    print str_lss
+    let work = qsort . map (foldr (+) 0 . map (read::String->Int) . split "\n") . split "\n\n"
+        (h1:h2:h3:_) = work content
+    print $ h1 + h2 + h3
